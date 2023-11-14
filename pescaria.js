d6119347549d3b21d7d9aa838869a53e5c316433
sarahtomazi <104771374+sarahtomazi@users.noreply.github.com>
2023-11-14 12:09:20 -0300

    Add files via upload

---
+++ pescaria.js
@@ -0,0 +1,19 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h4")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const pessoas = Number(frm.inNumero.value)
+    const peixe = Number(frm.inPeixe.value)
+    let resultado
+        if(peixe > pessoas) {
+            resultado = (pessoas * 20) + ((peixe - pessoas) * 12)
+        } else {
+            resultado = pessoas * 20
+        }
+
+resp.innerText = `N€ de pessoas: ${pessoas}\n
+                  N€ de peixes: ${peixe}\n
+                  Pagar: R$ ${resultado.toFixed(2)}`
+})
